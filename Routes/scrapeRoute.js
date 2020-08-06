5bb497e7a8e7914d02112de84f7bda944e36e2c4
Luís Ferro <lfaferro@live.com.pt>
2020-08-06 23:47:09 +0100

    removi ficheiros desnecessários

--- Routes/scrapeRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const scraperController = require('../Controllers/scraperController');
-    
-router.route('/:title').get(scraperController.getGame);
-router.route('/top/:platform').get(scraperController.getTop);
-
-module.exports = router;
